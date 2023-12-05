457fac189d666f6d774824b5e43659ac7d79ec07
ChibaweM <chibawemkwenya@gmail.com>
2023-12-05 12:31:01 +0200

    push

---
+++ android/app/src/main/kotlin/com/example/chat_app_nk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_app_nk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/user_model.dart
@@ -0,0 +1,11 @@
+class Users {
+  final String userName;
+  final String password;
+  final String email;
+
+  Users({
+    required this.userName,
+    required this.password,
+    required this.email,
+  });
+}
