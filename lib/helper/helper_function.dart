7197108e0565d28858daa007a1ef36591fc83dce
ChibaweM <chibawemkwenya@gmail.com>
2023-12-11 14:06:25 +0200

    update to initialising

---
+++ lib/helper/helper_function.dart
@@ -0,0 +1,7 @@
+class HelperFunctions {
+  //keys
+
+  static String userLoggedInKey = "UserLoggedInKey";
+  static String userNamrKey = "UserNameKey";
+  static String userEmailKey = "UserEmailKey";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
